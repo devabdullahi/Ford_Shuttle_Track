dd8f682e7ce6f1a8c0a08c57d885adbe9a423ca4
devabdullahi <minigunjummy320@gmail.com>
2025-06-30 15:14:05 -0400

    made a test main screen

--- App.js
+++ App.js
@@ -4,17 +4,19 @@ import LogInScreen from './Screens/LogIn';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './Navigation/RootNavigator';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import MainScreen from  './Screens/MainScreen'
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUpScreen" screenOptions={{ headerShown: false }}>
+      {/* <Stack.Navigator initialRouteName="SignUpScreen" screenOptions={{ headerShown: false }}>
           <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
           <Stack.Screen name="LogInScreen" component={LogInScreen} />
-          {/* Add other screens here */}
-      </Stack.Navigator>
+          Add other screens here
+      </Stack.Navigator> */}
+
+      <MainScreen/>
     </NavigationContainer> 
   );
 }  
