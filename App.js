af1df09595dd22d68adaf8f8aa6570baacfa1458
Rick H Nguyen <rick248nguyen@gmail.com>
2025-07-07 15:41:01 -0400

    Add firebase config, AuthContext

--- App.js
+++ App.js
@@ -4,17 +4,20 @@ import LogInScreen from './Screens/LogIn';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './Navigation/RootNavigator';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthProvider } from "./AuthContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUpScreen" screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-          <Stack.Screen name="LogInScreen" component={LogInScreen} />
-          {/* Add other screens here */}
-      </Stack.Navigator>
-    </NavigationContainer> 
+    <AuthProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="SignUpScreen" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+            <Stack.Screen name="LogInScreen" component={LogInScreen} />
+            {/* Add other screens here */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider> 
   );
 }  
