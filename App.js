61fad88ad7cdb6a435cf835e672463da3f2639d5
devabdullahi <minigunjummy320@gmail.com>
2025-07-29 15:40:45 -0400

    idk

--- App.js
+++ App.js
@@ -11,28 +11,22 @@ import StopDetailsScreen from './Screens/StopDetailScreen';
 import RequestStopScreen from './Screens/RequestStopsScreen';
 import MapScreen from './Screens/MapScreen';
 import FAQScreen from './Screens/FAQ';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    
     <NavigationContainer>
-      {
-        <MapScreen />
-        /*
       <Stack.Navigator initialRouteName="SignUpScreen" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
         <Stack.Screen name="LogInScreen" component={LogInScreen} />
         <Stack.Screen name="MainScreen" component={MainScreen} />
         <Stack.Screen name="ApprovedStops" component={ApprovedStopsScreen} />
-        <Stack.Screen name="StopDetails" component={StopDetailsScreen} screenOptions={{ headerShown: false }} />
+        <Stack.Screen name="StopDetails" component={StopDetailsScreen} />
         <Stack.Screen name="RequestStops" component={RequestStopScreen} />
         <Stack.Screen name="MapScreen" component={MapScreen} />
-        <Stack.Screen name="FAQScreen" component={FAQScreen}/>
+        <Stack.Screen name="FAQScreen" component={FAQScreen} />
       </Stack.Navigator>
-      */
-      }
     </NavigationContainer>
   );
-
 }
